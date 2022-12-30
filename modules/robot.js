851375dad293d932ef8158e6935f9d18c0cbff7c
Marcos Felippe Alves <mfelippe.bsb@hotmail.com>
2022-12-30 02:25:57 -0300

    as

--- modules/robot.js
+++ modules/robot.js
@@ -19,7 +19,7 @@ module.exports = function (app) {
         waitUntil: "networkidle2",
       });
 
-      await page.waitForSelector(".related-box", { timeout: 60000 });
+      await page.waitForSelector(".related-box", { timeout: 0 });
       var numerosSorteados = [];
 
       const anterior = "ul.clearfix> li>a";
