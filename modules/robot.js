0b39455261d400cc835a59a0cf732f75bfcd2393
Marcos Felippe Alves <mfelippe.bsb@hotmail.com>
2022-12-29 23:25:47 -0300

    api de buscar resultados da megasenna

---
+++ modules/robot.js
@@ -0,0 +1,57 @@
+const pupperteer = require("puppeteer");
+
+module.exports = function (app) {
+  app.get("/robot/api/test", async (req, res) => {
+    res.status(200).json("Api funcionando corretamente 🚀");
+  });
+
+  app.get("/robot/megasena/:qtd", async (req, res) => {
+    const { qtd } = req.params;
+    try {
+      const browser = await pupperteer.launch({
+        headless: true,
+        defaultViewport: null,
+      });
+
+      const page = await browser.newPage();
+
+      await page.goto("https://loterias.caixa.gov.br/Paginas/Mega-Sena.aspx", {
+        waitUntil: "networkidle2",
+      });
+
+      await page.waitForSelector(".related-box");
+      var numerosSorteados = [];
+
+      const anterior = "ul.clearfix> li>a";
+
+      // lopp para saber quantos resultados buscar
+      for (let index = 0; index < qtd; index++) {
+        // seleciona a lista de números de cada sorteio
+        const result = await page.evaluate(() => {
+          const dezenas = Array.from(
+            document
+              .querySelector("ul#ulDezenas")
+              .querySelectorAll("li.ng-binding")
+          ).map((el) => Number(el.innerHTML));
+
+          return dezenas;
+        });
+
+        // clica para o sorteio anterior
+        await page.click(anterior);
+
+        // aguarda os resultados os nº serem renderizados
+        await page.waitForSelector("li.ng-binding");
+
+        // salva os números encontrados
+        numerosSorteados.push(result);
+      }
+      await browser.close();
+
+      res.status(200).json({ sorteios: numerosSorteados });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "ops ocorreu um erro" });
+    }
+  });
+};
