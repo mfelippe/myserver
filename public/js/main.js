a98d40ab5b1a76c3853a21816b1cfcbef8d17ffa
Marcos Felippe Alves <mfelippe.bsb@hotmail.com>
2023-02-10 17:47:33 -0300

    nova estrutura de servidor proprio

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-function sendMessage() {
-  const mensagem = document.getElementById("exampleFormControlTextarea1");
-  const contato = 61993153532;
-  const link = `https://api.whatsapp.com/send?phone=55${contato}&text=${mensagem.value}`;
-
-  window.open(link, "_blank");
-}
